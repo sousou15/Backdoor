3bd5ce6a222490c9acfaa2ff6c91c8f40babe7bb
Youssef <43934641+sousou15@users.noreply.github.com>
2024-02-28 16:15:07 +0100

    Update hello.bat

--- Hermes/hello.bat
+++ Hermes/hello.bat
@@ -1,4 +1,4 @@
 @echo off
 
-rem Abre una nueva ventana de la línea de comandos y ejecuta la simulación de la pantalla de hacker.
-start cmd /k "cls && echo Simulación de pantalla de hacker... && timeout /t 2 >nul && color 0A && for /l %%i in (1,1,10000) do (set /a rand=!random! %% 94 + 33 && <nul set /p =!rand! && timeout /t 0 >nul) && echo. && echo Acceso concedido. && pause >nul"
+
+start cmd /k "cls && echo... && timeout /t 2 >nul && color 0A && for /l %%i in (1,1,10000) do (set /a rand=!random! %% 94 + 33 && <nul set /p =!rand! && timeout /t 0 >nul) && echo. && echo Acceso concedido. && pause >nul"
